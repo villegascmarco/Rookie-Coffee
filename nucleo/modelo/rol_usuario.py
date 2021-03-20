bb1704b3d88f4db16a52e9e38e11333e1e5225bd
MoiMorua <57070474+MoiMorua@users.noreply.github.com>
2021-03-20 11:04:14 -0600

    se agregan archivos de modelo

---
+++ nucleo/modelo/rol_usuario.py
@@ -0,0 +1,12 @@
+from . import db
+from flask_sqlalchemy import SQLAlchemy
+
+class Rol_usuario(db.Model):
+    __tablename__="rol_usuario"
+    _id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(45), nullable=False)
+    descripcion = db.Column(db.String(100), nullable=False)
+    estatus = db.Column(db.String(64), nullable=False,default='Activo')
+    
+
+
