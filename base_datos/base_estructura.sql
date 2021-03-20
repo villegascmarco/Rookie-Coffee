016ac5656c6db0f4497ed74b9c5183b475d8696f
Villegas <villegascmarco@gmail.com>
2021-03-20 11:56:59 -0600

    First dump added

--- base_datos/base_estructura.sql
+++ base_datos/base_estructura.sql
@@ -71,7 +71,7 @@ DROP TABLE IF EXISTS `ingrediente_producto`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ingrediente_producto` (
-  `_id` int NOT NULL,
+  `_id` int NOT NULL AUTO_INCREMENT,
   `cantidad_requerida` double NOT NULL,
   `producto` int NOT NULL,
   `ingrediente` int NOT NULL,
